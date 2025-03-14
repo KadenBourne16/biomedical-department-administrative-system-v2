8934dc66181e9b6a5fc99378b79bf5975c3aa3b6
Kaden16 <78388758+KadenBourne16@users.noreply.github.com>
2025-03-14 14:58:19 +0000

    Update env.js

--- src/sanity/env.js
+++ src/sanity/env.js
@@ -1,5 +1,4 @@
-export const apiVersion =
-  process.env.NEXT_PUBLIC_SANITY_API_VERSION || '2025-03-13'
+export const apiVersion = process.env.NEXT_PUBLIC_SANITY_API_VERSION || '2025-03-13'
 
-export const dataset = process.env.NEXT_PUBLIC_SANITY_DATASET;
-export const projectId = process.env.NEXT_PUBLIC_SANITY_PROJECT_ID;
+export const dataset = "bdas";
+export const projectId = "t8e4ngk6";
