1b29ae8aab38af43efd214503961dc177d126bde
Kaden16 <78388758+KadenBourne16@users.noreply.github.com>
2025-03-17 12:37:22 +0000

    Update client.js

--- src/sanity/lib/client.js
+++ src/sanity/lib/client.js
@@ -1,10 +1,10 @@
 import { createClient } from 'next-sanity'
 
 export const client = createClient({
-  projectId: process.env.NEXT_PUBLIC_SANITY_PROJECT_ID,
-  dataset: process.env.NEXT_PUBLIC_SANITY_DATASET,
-  apiVersion: "2023-05-03", // Use the latest API version
-  token: process.env.NEXT_PUBLIC_SANITY_API_TOKEN, // This should be a server-side env variable in production
-  useCdn: false, // We need to use the API directly for mutations
+  projectId: t8e4ngk6,
+  dataset: bdas,
+  apiVersion: "2023-05-03", 
+  token: "skcasFAvJwJXCS8O2PxpBFtljK4aDML13z5vIHRFdHTGdsJGUoQfl0q5wooel1emZXppfv6TJbk4EahbixD0HSKi5i81VggGusy3Rdfj1QEFrcF6J9uOvlJHPSWuO3WVo1ie6XVLAi4hrbeYyXacJYtIY9SKdxZgwHKsGILmWIBdeHRVR2IT", 
+  useCdn: false,
 })
 
